0892f52cb6a38921219e3a1e268d506d374be16b
pekstaar <pekstaar@gmail.com>
2022-01-17 23:04:20 +0300

    Project 1

---
+++ app/src/main/java/ke/co/tukenya/ctit2019_project/MainActivity.kt
@@ -0,0 +1,11 @@
+package ke.co.tukenya.ctit2019_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
